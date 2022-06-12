03e1a330eb9f9aee15cb237e83a4becce5bd1706
kyler <kyler1418@outlook.com>
2022-06-12 10:50:52 -0400

    commit tree b32e57bc46bace2b78b33ee2d74752c50b22f1e2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function mySubtract(number) {
+    if (number > 11) {
+      return number - 11;
+    } else {
+      return 11 - number;
+    }
+    return false;
+  }
+  
+  mySubtract(100);
+  
